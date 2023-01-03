5320f6fa7eebbb11c7f8e9f52062ac21faba1203
MurukLuan <murukluan@gmail.com>
2023-01-03 11:05:58 -0300

    primeiro commit com o projeto criado e a primeira classe criada tambem, com um padrão rest implementado

---
+++ Controllers/FilmeController.cs
@@ -0,0 +1,41 @@
+﻿using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesAPI.Controllers;
+
+[ApiController]
+[Route("[Controller]")]
+public class FilmeController : ControllerBase
+{
+    private static List<Filme> filmes = new List<Filme>();
+    private static int id = 0;
+
+    [HttpPost]
+    public IActionResult AdicionaFilme([FromBody] Filme filme)
+    {
+        //adciona um filme com um id
+        filme.Id = id++;
+        filmes.Add(filme);
+        //cria o filme e redireciona para o caminho com o id que foi criado
+        return CreatedAtAction(nameof(RecuperaFilmePorId), new { id = filme.Id }, filme);
+    }
+
+    [HttpGet]
+    //busca todos os filmes, com skip(pula) como padrão 0, e take(pega) como padrão 10
+    public IEnumerable<Filme> RecuperaFilmes([FromQuery] int skip = 0, [FromQuery] int take = 10)
+    {
+        return filmes.Skip(skip).Take(take);
+    }
+
+    [HttpGet("{id}")]
+    //recupera um filme por id que é passado como parametro
+    public IActionResult RecuperaFilmePorId(int id)
+    {
+        //pega o primeiro filme da lista com o id solicitado
+        var filme = filmes.FirstOrDefault(filme => filme.Id == id);
+        //caso o retorno for um id que não há na base, retorna falso e devolve uma ação 404 (not found)
+        if (filme == null) return NotFound();
+        //caso contrario retorna os dados do filme com o status 200(ok)
+        return Ok(filme);
+    }
+}
---
+++ Models/Filme.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Models;
+
+public class Filme
+{
+    public int Id { get; set; }
+
+    [Required(ErrorMessage ="O titulo do filme é obrigatório")]
+    public string Titulo { get; set; }
+
+    [Required(ErrorMessage = "O genero do filme é obrigatório")]
+    [MaxLength(50, ErrorMessage = "o tamanho maximo do gênero é 50 caracteres")]
+    public string Genero { get; set; }
+
+    [Required(ErrorMessage = "A duração do filme é obrigatório")]
+    [Range(70, 600, ErrorMessage ="a druação deve estar entre 70 e 600 minutos")]
+    public int Duracao { get; set; }
+}
